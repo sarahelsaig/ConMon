c4a9dbf6f15075af1bbe13409af78d76211c3e2c
DAud_IcI <daud.ici@gmail.com>
2020-02-23 19:53:10 +0100

    reorganize startup

--- ConMon/Startup.cs
+++ ConMon/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Transactions;
 
 namespace ConMon
 {
@@ -30,15 +29,29 @@ namespace ConMon
 
         public IConfiguration Configuration { get; }
 
+        private Action<DbContextOptionsBuilder> CreateDbConnection(string connectionString) =>
+            new Action<DbContextOptionsBuilder>(options =>
+            {
+                switch (Configuration.GetConnectionType())
+                {
+                    case ConnectionType.SqlServer: options.UseSqlServer(connectionString); break;
+                    case ConnectionType.MySql: options.UseMySql(connectionString); break;
+                    case ConnectionType unknown: throw new InvalidOperationException($"Unknown or unsupported connection type: {unknown}");
+                }
+            });
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             var schedulerConnectionString = Configuration.GetConnectionString("Scheduler");
 
+
             services.AddSingleton(Configuration);
             services.AddSingleton(ProgramAlias.DictionaryFromConfiguration(Configuration));
             services.AddScoped<IApplicationService, ApplicationService>();
             services.AddScoped<IRunAs, RunAs>();
+
+
             services.AddHangfire(configuration => {
                 switch (Configuration.GetConnectionType())
                 {
@@ -47,15 +60,8 @@ namespace ConMon
                     case ConnectionType unknown: throw new InvalidOperationException($"Unknown or unsupported connection type: {unknown}");
                 }
             });
-            services.AddDbContext<SchedulerContext>(options =>
-            {
-                switch (Configuration.GetConnectionType())
-                {
-                    case ConnectionType.SqlServer: options.UseSqlServer(schedulerConnectionString); break;
-                    case ConnectionType.MySql: options.UseMySql(schedulerConnectionString); break;
-                    case ConnectionType unknown: throw new InvalidOperationException($"Unknown or unsupported connection type: {unknown}");
-                }
-            });
+            services.AddDbContext<SchedulerContext>(CreateDbConnection(schedulerConnectionString));
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
@@ -78,8 +84,12 @@ namespace ConMon
                 Authorization = new[] { new Filters.HangfireAuthorizationFilter() },
             });
 
-            app.UseHttpsRedirection();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            //app.UseHttpsRedirection();
             app.UseRouting();
+            //app.UseAuthentication();
+            //app.UseAuthorization();
             app.UseEndpoints(endpoints => 
             {
                 endpoints.MapControllers();
@@ -87,14 +97,9 @@ namespace ConMon
                 //endpoints.MapHub<MyChatHub>();
                 //endpoints.MapGrpcService<MyCalculatorService>();
             });
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
 
-            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
-            using (var context = scope.ServiceProvider.GetService<SchedulerContext>())
-            {
-                context.Database.Migrate();
-            }
+            using var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            using (var context = scope.ServiceProvider.GetService<SchedulerContext>()) context.Database.Migrate();
         }
     }
 }
